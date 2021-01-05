40685e7a50a953d3c1b6b9afc62f018d1bcfa7b8
jasonemarkson <jasonmarkson76@gmail.com>
2021-01-04 22:14:06 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,12 @@ class Post < ActiveRecord::Base
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :title_clickbait
     #validates :non_clickbait, inclusion: {:in %w(Won't Believe Secret Top [number] Guess), message: "Click Bait"}
-    include ActiveModel::Validations
-    validates_with TitleValidator
+    
+    def title_clickbait
+        if title && title.exclude?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+          errors.add :title, "Title is click-baity"
+        end
+    end
 end
--- app/validators/title_validator.rb
+++
@@ -1,7 +0,0 @@
-class TitleValidator < ActiveRecord::Validator
-    def validate(record)
-        if record.title.include?("Won't Believe", "Secret", "Top [number]", "Guess")
-            record.errors[:title] << "Title is click-baity"
-        end
-    end
-end
