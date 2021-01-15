cceb64ef467ff066626a70032af65f4ac609b438
tonitdiep <adiep123@gmail.com>
2021-01-14 21:39:57 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, presence: true, length: { maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait?
+#singular call on the method
+#plural validate helpers
+    CLICKBAIT_PATTERN =
+  [ /Won't Believe/, /Secret/, /Top [number]/, /Guess/ ]
+
+        def is_clickbait?
+            if CLICKBAIT_PATTERN.none? {|pattern| pattern.match title}
+                errors.add(:title, "possible clickbait")
+
+            end
+        end
+  
+
+
+
+
+
 end
